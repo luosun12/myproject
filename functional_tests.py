cfce1d74a11e9cd5d247abe8f68ceaa36d119a77
luosun12 <2859177873@qq.com>
2025-03-25 14:21:56 +0800

    message 5: The first iterative development

--- functional_tests.py
+++ functional_tests.py
@@ -14,7 +14,7 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         # 检查网页标题是否包含"To-Do"
-        self.assertEqual("To-Do", self.browser.title, "browser title was:" + self.browser.title)
+        self.assertEqual("To-Do lists", self.browser.title, "browser title was:" + self.browser.title)
         self.fail("Finish the test!")
 
         # 应用有一个输入待办事项的文本输入框
