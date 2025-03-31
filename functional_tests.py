a3da18f1ff75101e2099c48351921912439fdc6a
luosun12 <2859177873@qq.com>
2025-03-31 11:21:19 +0800

    message 9: After refactoring the functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 用户听说有一个在线待办事项应用
         # 他去打开了这个应用的首页
@@ -29,15 +34,21 @@ class NewVisitorTest(unittest.TestCase):
             'Enter a to-do item'
         )
 
-        # 他在文本输入框中输入了 "Buy flowers"
+        # 他按了回车键后，页面更新了
+        inputbox.send_keys('Buy flowers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy flowers')
+
+        # 页面中又显示了一个文本输入框，可以输入其他待办事项"
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Buy flowers', [row.text for row in rows])
-        self.assertIn('2: Give a gift to Lisi', [row.text for row in rows])
+        #页面再次更新，他的清单中显示了这两个待办事项
+        self.check_for_row_in_list_table('1: Buy flowers')
+        self.check_for_row_in_list_table('2: Give a gift to Lisi')
 
         self.fail('Finish the test!')
         # 他访问的页面URL，发现他的待办事项列表还在
