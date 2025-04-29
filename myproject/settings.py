ffeaf103c759e9461df4e779986020d422e2f96f
luosun12 <2859177873@qq.com>
2025-04-29 17:30:47 +0800

    message 22: use Bootstrap to improve layout

--- myproject/settings.py
+++ myproject/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
+STATIC_URL = '/static/'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-nv+9%hm$4&xo2#^8v@(xu4p$z0cw#1rt#2x7k)xfr6lvc9s13r'
